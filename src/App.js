4c451fb4fdc5c797eef6f528f151bb5cca8845c3
Alois Niedermaier <a.niedermaier@techdivision.com>
2020-02-06 16:24:41 +0100

    adding cookie session auth

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Board from './components/Board';
 import Archiv from './components/Archiv';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
+import axios from 'axios';
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    authenticate();
+  }, []);
+
+  const authenticate = async () => {
+    let response = await axios.get('http://localhost:8001/authenticate');
+    if(response.status === 200) {
+      setAuthenticated(true);
+    }
+  }
+
+  const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props) => (
+      authenticated === true
+        ? <Component {...props} />
+        : <Redirect to='/login' />
+    )} />
+  )
+
   return (
     <div className="App">
-    <Router>
-      <div>
+      <Router>
         <Switch>
-          <Route path="/archiv">
-            <Archiv></Archiv>
-          </Route>
-          <Route path="/">
-            <Board></Board>
-          </Route>
+          <PrivateRoute path="/board" component={Board} />
+          <PrivateRoute path="/login" component={Board} />
+          <PrivateRoute path="/archiv" component={Archiv} />
+          <PrivateRoute path="/" exact component={Board} />
         </Switch>
-      </div>
-    </Router>
+      </Router>
     </div>
   );
 }
